ec0b3e9434d1cf470c2a03cfdb625dc49a98f322
Sofia13066 <podolskaya.sofya190498@gmail.com>
2022-10-05 23:40:21 +0300

    modified:   Products.sql

--- Products.sql
+++ Products.sql
@@ -8,3 +8,16 @@ SELECT name, price, quantity FROM products ORDER BY price DESC LIMIT 3;
 SELECT SUM(quantity) AS `Total quantity`, SUM(quantity * price) AS `Total cost`, (SUM(price)/8) AS `Average price` FROM products;
 SELECT DISTINCT productCode FROM products;
 SELECT CONCAT(productCode, ' - ', name) AS 'Product description', price AS `Item Price`, quantity AS 'Quantity', (price * quantity) AS 'Cost' FROM products ORDER BY cost;
+
+
+-----new HW_58
+SELECT * FROM products p, suppliers s WHERE s.name = 'ABC Traders';
+SELECT SUM(quantity * price) AS `Total Cost` FROM products p, suppliers s WHERE s.name IN('XYZ Company', 'QQ Corp');
+SELECT s.name, CAST(SUM(quantity * price)/SUM(quantity) AS DECIMAL(7,2)) AS `Average price` FROM products p, suppliers s WHERE
+p.supplierID = s.supplierID GROUP BY s.name;
+SELECT s.name AS `Supplier Name`, s.phone AS `Supplier Phone`, GROUP_CONCAT(p.name) AS `Product Names` FROM products p, suppliers s WHERE 
+p.supplierID = s.supplierID GROUP BY p.supplierID ORDER BY COUNT(p.name) DESC LIMIT 1;
+SELECT s.name AS `Name`, s.phone AS `Phone`, p.price AS `Marker price` FROM products p, suppliers s WHERE 
+p.supplierID = s.supplierID AND p.productCode='MRK' ORDER BY p.price LIMIT 1;
+SELECT s.name AS `Supplier Name`, s.phone AS `Supplier Phone`, COUNT(p.name) AS `Number Of Product Codes` FROM products p, suppliers s WHERE 
+p.supplierID = s.supplierID GROUP BY p.supplierID ORDER BY COUNT(p.name) DESC LIMIT 1;
